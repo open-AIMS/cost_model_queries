fa58cdb236f2d0232dbb93a0d1269fa60d73305b
Rose Crocker <r.crocker@aims.gov.au>
2024-11-07 08:29:00 +1000

    Add function imports from `sampling_functions.py` to SA files

--- SA-deployment-cost-model.py
+++ SA-deployment-cost-model.py
@@ -3,37 +3,24 @@ import pandas as pd
 
 from SALib.analyze.sobol import analyze
 from SALib.sample.sobol import sample
-from SALib import ProblemSpec
 
 import matplotlib.pyplot as plt
 
-samples_fn ="CAD_model_samples_run2.csv"
+from sampling_functions import deployment_problem_spec
+
+# Load samples for deployment cost
+samples_fn ='deployment_cost_samples.csv'
 samples_df = pd.read_csv(samples_fn)
 
-factor_dict = {'num_devices': [1.0, 100000000.0],
-            'port': [0.0, 4.0],
-            'deployment_dur': [20.0, 28.0],
-            'DAJ_a_r': [1.0, 4.0],
-            'DAJ_c_s': [1.0, 4.0],
-            'deck_space': [8.0, 15.0],
-            'distance_from_port': [13.0, 200.0],
-            '1YOEC_yield': [0.6, 0.8],
-            'secs_per_dev': [0.0, 4.0],
-            'bins_per_tender': [3.0, 7.0],
-            'proportion': [0.4,0.6],
-            'cape_ferg_price':[12000, 15000],
-            'ship_endurance': [12.0, 16.0]}
-
-is_cat = [True, True, True, True, True, True, False, False, False, True, False, False, True]
-factor_names = [*factor_dict.keys()]
-problem_dict = {'num_vars': len(is_cat), 'names': factor_names, 'bounds':[*factor_dict.values()]}
-sp = ProblemSpec(problem_dict)
+# Get deployment problem spec
+sp, factor_names, is_cat = deployment_problem_spec()
 sp.samples=np.array(samples_df[factor_names])
 
 # First get sensitivity to setup cost
 sp.set_results(np.array(samples_df['setupCost']))
 sp.analyze_sobol()
 
+# Plot sensitivities
 axes = sp.plot()
 axes[0].set_yscale('log')
 fig = plt.gcf()  # get current figure
@@ -41,6 +28,7 @@ fig.set_size_inches(10, 4)
 plt.tight_layout()
 plt.show()
 
+# Get pawn sensitvities
 sp.analyze_pawn()
 axes = sp.plot()
 fig = plt.gcf()  # get current figure
@@ -48,68 +36,14 @@ fig.set_size_inches(10, 4)
 plt.tight_layout()
 plt.show()
 
+# Plot pawn sensitivities  as a heat map
 # SALib.analyze.rsa.analyze(problem_dict, sp.samples, total_cost)
 sp.heatmap()
 plt.show()
 
 # Then get sensitivity to operational cost
-sp = ProblemSpec(problem_dict)
 sp.samples=np.array(samples_df[factor_names])
-
-# First get sensitivity to setup cost
 sp.set_results(np.array(samples_df['Cost']))
-sp.analyze_sobol()
-
-axes = sp.plot()
-axes[0].set_yscale('log')
-fig = plt.gcf()  # get current figure
-fig.set_size_inches(10, 4)
-plt.tight_layout()
-plt.show()
-
-sp.analyze_pawn()
-axes = sp.plot()
-fig = plt.gcf()  # get current figure
-fig.set_size_inches(10, 4)
-plt.tight_layout()
-plt.show()
-
-# SALib.analyze.rsa.analyze(problem_dict, sp.samples, total_cost)
-sp.heatmap()
-plt.show()
-
-# Then get sensitivity to operational cost
-sp = ProblemSpec(problem_dict)
-sp.samples=np.array(samples_df[factor_names])
-
-# First get sensitivity to setup cost
-sp.set_results(np.array(samples_df['setupCost_percoral']))
-sp.analyze_sobol()
-
-axes = sp.plot()
-axes[0].set_yscale('log')
-fig = plt.gcf()  # get current figure
-fig.set_size_inches(10, 4)
-plt.tight_layout()
-plt.show()
-
-sp.analyze_pawn()
-axes = sp.plot()
-fig = plt.gcf()  # get current figure
-fig.set_size_inches(10, 4)
-plt.tight_layout()
-plt.show()
-
-# SALib.analyze.rsa.analyze(problem_dict, sp.samples, total_cost)
-sp.heatmap()
-plt.show()
-
-# Then get sensitivity to operational cost
-sp = ProblemSpec(problem_dict)
-sp.samples=np.array(samples_df[factor_names])
-
-# First get sensitivity to setup cost
-sp.set_results(np.array(samples_df['Cost_percoral']))
 sp.analyze_sobol()
 
 axes = sp.plot()
--- SA-production-cost-model.py
+++ SA-production-cost-model.py
@@ -1,35 +1,17 @@
 import numpy as np
 import pandas as pd
 
-# import SALib
 from SALib.analyze.sobol import analyze
 from SALib.sample.sobol import sample
-from SALib import ProblemSpec
 
 import matplotlib.pyplot as plt
 
-samples_fn ="prod_model_samples_run2.csv"
+from sampling_functions import production_problem_spec
+
+samples_fn = 'production_cost_samples.csv'
 samples_df = pd.read_csv(samples_fn)
 
-factor_dict = {'num_devices': [1.0, 100000000.0],
-                'colony_hold_time': [19.0, 23.0],
-                'species_no': [8.0, 16.0],
-                'col_spawn_gam_bun': [57600, 70400],
-                'gam_bun_egg': [10.0, 14.0],
-                'egg_embryo': [0.7, 0.9],
-                'embryo_freeswim': [0.7, 0.9],
-                'freeswim_settle': [0.7,0.9],
-                'settle_just': [0.7, 0.9],
-                'just_unit': [6.0, 9.0],
-                'just_mature': [0.6, 0.9],
-                '1YOEC_yield': [0.6, 0.8],
-                'optimal_rear_dens': [1.0 , 3.0]}
-is_cat = [True, True, True, True, True, False, False, False, False, True, False, False, True]
-
-factor_names = [*factor_dict.keys()]
-problem_dict = {'num_vars': len(is_cat), 'names': factor_names, 'bounds':[*factor_dict.values()]}
-sp = ProblemSpec(problem_dict)
-
+sp, factor_names, is_cat = production_problem_spec()
 sp.samples=np.array(samples_df[factor_names])
 
 # First get sensitivity to setup cost
@@ -55,55 +37,10 @@ sp.heatmap()
 plt.show()
 
 # Then get sensitivity to operational cost
-sp = ProblemSpec(problem_dict)
 sp.samples=np.array(samples_df[factor_names])
 
 # Get sensitivity to operational cost
 sp.set_results(np.array(samples_df['Cost']))
-sp.analyze_sobol()
-
-axes = sp.plot()
-axes[0].set_yscale('log')
-fig = plt.gcf()  # get current figure
-fig.set_size_inches(10, 4)
-plt.tight_layout()
-plt.show()
-
-sp.analyze_pawn()
-axes = sp.plot()
-fig = plt.gcf()  # get current figure
-fig.set_size_inches(10, 4)
-plt.tight_layout()
-plt.show()
-
-# SALib.analyze.rsa.analyze(problem_dict, sp.samples, total_cost)
-sp.heatmap()
-plt.show()
-
-# Get sensitivity to setup cost per coral
-sp.set_results(np.array(samples_df['setupCost_percoral']))
-sp.analyze_sobol()
-
-axes = sp.plot()
-axes[0].set_yscale('log')
-fig = plt.gcf()  # get current figure
-fig.set_size_inches(10, 4)
-plt.tight_layout()
-plt.show()
-
-sp.analyze_pawn()
-axes = sp.plot()
-fig = plt.gcf()  # get current figure
-fig.set_size_inches(10, 4)
-plt.tight_layout()
-plt.show()
-
-# SALib.analyze.rsa.analyze(problem_dict, sp.samples, total_cost)
-sp.heatmap()
-plt.show()
-
-# Get sensitivity to operational cost per coral
-sp.set_results(np.array(samples_df['Cost_percoral']))
 sp.analyze_sobol()
 
 axes = sp.plot()
